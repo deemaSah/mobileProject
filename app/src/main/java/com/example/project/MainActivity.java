dc09f4c9eb7c35823f88887e0dbe11617e3c12dc
deemaSah <deemasahyoni@gmail.com>
2021-12-14 00:55:49 +0200

    version 1.0

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void LogIn(View view) {
+        Intent intent =new Intent(this,LogInActivity.class);
+        startActivity(intent);
+    }
+}
